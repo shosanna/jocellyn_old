07a1141aaaeef174dbe599bf026422c7817e6a69
Jakub Arnold <darthdeus@gmail.com>
2013-02-02 13:48:14 +0100

    Initial stuffz

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
 // Application bootstrapper
 
-module.exports = Em.Application.create();
+module.exports = Em.Application.create({
+  ready: function() {
+    setTimeout(function() {
+    $(".item.small").hover(function() {
+      $(this).animate({ width: 130 }, 200);
+    }, function() {
+      $(this).animate({ width: 100}, 200);
+    });
+
+    }, 500);
+  }
+});
