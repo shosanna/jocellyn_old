9034b884ca4e136337d8a3f4a1f51782917f7fb7
Zuzana Dostalova <sanna23@icloud.com>
2013-02-11 22:06:38 +0100

    Tom project finished II

--- projects/Tom/script.js
+++ projects/Tom/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
+    $(".selection").hide();
     var date = prompt("Jaké je dnes datum? Napiš to ve formátu - 1. května = 1/5");
     if (date === "12/2") {
-      document.location = "main.html";
+      $(".selection").fadeIn(900);
     } else {
     $("body").css("background-image", "url(img/error.jpg)");
     }
-
+    $(".wrong").click(function(){
+      $(".selection").hide(), $("body").css("background-image", "url(img/error.jpg)");
+    });
+    $(".correct").click(function(){
+      alert("Ano! Potvrdil jsi svou totožnost, jsi Ijáček a můžeš pokračovat...");
+      document.location = "main.html";
+    });
 });
 
 
--- projects/Tom/script3.js
+++ projects/Tom/script3.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
 
+  // vytvori cestu k obrazku, kterou si slozi za pomoci p a jeho data atributu.
   function findImage(p) {
     var selector = "img[src='img/" + p.data("image") + ".jpg']";
     return $(selector);
   }
 
+  // Po najeti mysi na odstavec p se zobrazi obrazek
   $("p").hover(function(e) {
     $(this).css("color", "#155F98");
 
-    var image = findImage($(this));
-
-    image.fadeIn().css({
-      top: e.pageY,
-      left: e.pageX + 500,
+    findImage($(this)).fadeIn().css({
+      top: e.pageY - 100,
+      left: e.pageX + 200,
       position: "absolute"
     });
 
   }, function() {
-    $(this).css("color", "#777A8F");
-    $(".img2").hide();
+    $("p").css("color", "#777A8F");
+    findImage($(this)).hide();
   });
 
   $("p").mousemove(function(e) {
     var image = findImage($(this));
 
     image.css({
-      top: e.pageY,
-      left: e.pageX + 500
+      top: e.pageY - 100,
+      left: e.pageX + 200
     });
   });
 
